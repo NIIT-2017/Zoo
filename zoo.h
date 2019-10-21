d051f76e55f612233646a1ecb277a71ebbac0b87
Anton A. Shtanyuk <ashtanyuk@gmail.com>
2019-10-21 10:59:25 +0300

    Update zoo.h

--- zoo.h
+++ zoo.h
@@ -2,16 +2,16 @@
 
 struct Animal
 {
-	char type[64]; // что за животное
-	char name[64]; // кличка
-	int age;   // возраст
-	int danger;  // опасно ли?
-	struct Animal *next; // след. элемент списка
+	char type[64]; 
+	char name[64]; 
+	int age;  
+	int danger;  
+	struct Animal *next; 
 };
 
 typedef struct Animal TAnimal;
 
-// создание списка
+
 TAnimal* create();
 void     addTail(TAnimal *head);
 void     printList(TAnimal *head);
